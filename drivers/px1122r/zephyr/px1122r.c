cc7940b6b87a527b9593f42a102a964e19cd890b
John Donovan <john@geospark.co.uk>
2023-02-18 15:52:54 +0000

    We have a device that sits in the tree.

--- drivers/px1122r/zephyr/px1122r.c
+++ drivers/px1122r/zephyr/px1122r.c
@@ -1,3 +1,63 @@
-int foo() {
-    return 100;
+#include "px1122r.h"
+
+#include <zephyr/drivers/uart.h>
+#include <zephyr/types.h>
+#include <zephyr/logging/log.h>
+
+#define LOG_LEVEL CONFIG_PX1122R_LOG_LEVEL
+#define DT_DRV_COMPAT skytraq_px1122r
+
+LOG_MODULE_REGISTER(PX1122R, LOG_LEVEL);
+
+static void print_impl(const struct device *dev);
+
+static const struct px1122r_driver_api px1122r_api = {
+	.print = print_impl
+};
+
+
+static struct px1122r_dev_data {
+	uint32_t foo;
+} data;
+
+static int init(const struct device *dev)
+{
+	// data.foo = 5;
+
+	return 0;
 }
+
+void px1122r_print(const struct device *dev) {
+	const struct px1122r_driver_api *api = dev->api;
+
+	__ASSERT(api->print, "Callback pointer should not be NULL");
+
+	api->print(dev);
+}
+
+static void print_impl(const struct device *dev)
+{
+	LOG_INF("Hello World from the kernel: %d", data.foo);
+
+	__ASSERT(data.foo == 5, "Device was not initialized!");
+}
+
+#define PX1122R_DEFINE(inst)							\
+	static struct px1122r_dev_data px1122r_dev_data_##inst = {			\
+		.foo = 5,				\
+	};									\
+	DEVICE_DT_INST_DEFINE(inst,						\
+						  init, NULL,                              \
+						  &px1122r_dev_data_##inst, NULL, \
+						  POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY,             \
+						  &px1122r_api);
+
+
+	// static const struct counter_config_info pcf85063_cfg_info_##inst = {	\
+	// 	.max_top_value = 0xff,						\
+	// 	.freq = 1,							\
+	// 	.channels = 1,							\
+	// };									\
+
+/* Create the struct device for every status "okay"*/
+DT_INST_FOREACH_STATUS_OKAY(PX1122R_DEFINE)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
-#include <zephyr/kernel.h>
+#include "px1122r.h"
 
-void main(void)
-{
-	printk("Hello World! %s\n", CONFIG_BOARD);
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(DIY_GNSS_V2, LOG_LEVEL_DBG);
+const struct device* dev = DEVICE_DT_GET(DT_INST(0, skytraq_px1122r));
+
+void main(void) {
+#if (__ASSERT_ON == 0)
+	if (dev == NULL) {
+		LOG_ERR("Failed to get device binding for PX1122R");
+		return;
+	}
+#endif
+	__ASSERT(dev, "Failed to get device binding");
+
+	LOG_INF("Initializing device %s (@%p)", dev->name, dev);
+	px1122r_print(dev);
 }
